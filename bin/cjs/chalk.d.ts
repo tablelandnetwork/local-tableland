c73cbb035bcbd335e818b0f3c815b07ab05c807a
Joe Wagner <njwjs722@gmail.com>
2022-09-23 15:54:28 -0600

    Remove bin from version control

--- bin/cjs/chalk.d.ts
+++
@@ -1,8 +0,0 @@
-interface Chalk {
-    (text: string): string;
-    bold: (text: string) => string;
-}
-export declare const chalk: {
-    [key: string]: Chalk;
-};
-export {};
--- bin/tableland.config.example.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    validatorDir: "tableland-artifacts/go-tableland",
-    registryDir: "tableland-artifacts/evm-tableland",
-    verbose: false,
-};
-export default config;
-//# sourceMappingURL=tableland.config.example.js.map
