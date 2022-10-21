83f64985888813fe40cc2385b464c21b8889fff5
Joe Wagner <njwjs722@gmail.com>
2022-10-21 12:48:38 -0600

    fix and cleanup evm-tableland dep and deploy

--- registry/hardhat.config.ts
+++ registry/hardhat.config.ts
@@ -1,6 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, extendEnvironment } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import { baseURIs, proxies, TablelandNetworkConfig } from "@tableland/evm/network";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -12,7 +15,9 @@ const config: HardhatUserConfig = {
         interval: [100, 3000],
       },
     },
-  }
+  },
+  baseURIs,
+  proxies,
 };
 
 declare module "hardhat/types/runtime" {
@@ -25,6 +30,32 @@ declare module "hardhat/types/runtime" {
 extendEnvironment((hre: HardhatRuntimeEnvironment) => {
   // Set base URI for local network
   hre.baseURI = "http://localhost:8080/chain/31337/tables/";
+});
+
+declare module "hardhat/types/config" {
+  // eslint-disable-next-line no-unused-vars
+  interface HardhatUserConfig {
+    baseURIs: TablelandNetworkConfig;
+    proxies: TablelandNetworkConfig;
+  }
+}
+
+declare module "hardhat/types/runtime" {
+  // eslint-disable-next-line no-unused-vars
+  interface HardhatRuntimeEnvironment {
+    baseURI: string;
+    proxy: string;
+  }
+}
+
+extendEnvironment((hre: HardhatRuntimeEnvironment) => {
+  // Get base URI for user-selected network
+  const uris = hre.userConfig.baseURIs as any;
+  hre.baseURI = uris[hre.network.name];
+
+  // Get proxy address for user-selected network
+  const proxies = hre.userConfig.proxies as any;
+  hre.proxy = proxies[hre.network.name];
 });
 
 export default config;
