846bea56287cd14fb3e6790256586bdb797e73cb
Joe Wagner <njwjs722@gmail.com>
2022-10-26 16:53:27 -0600

    Fixes to prettier

--- registry/scripts/deploy.ts
+++ registry/scripts/deploy.ts
@@ -1,9 +1,9 @@
 import { ethers } from "hardhat";
 
 async function main() {
-console.log("Importing the deploy script");
+  console.log("Importing the deploy script");
   await import("@tableland/evm/scripts/deploy.js");
-console.log("Imported the deploy script");
+  console.log("Imported the deploy script");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
