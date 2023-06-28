1503e2fefb8b6e72f31625b1fa09df77fc184d12
Joe Wagner <njwjs722@gmail.com>
2023-06-28 15:52:35 -0600

    Fix linting

--- test/fork.test.mjs
+++ test/fork.test.mjs
@@ -1,8 +1,5 @@
 import chai from "chai";
-import {
-  getAccounts,
-  getDatabase,
-} from "../dist/esm/util.js";
+import { getAccounts, getDatabase } from "../dist/esm/util.js";
 import { LocalTableland } from "../dist/esm/main.js";
 
 const expect = chai.expect;
@@ -14,7 +11,7 @@ describe("Starting a Fork", function () {
     silent: true,
     fork: "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY,
     // Adding a specific block number should enable caching
-    forkBlockNumber: 17560854
+    forkBlockNumber: 17560854,
   });
 
   // need to allow the whole history to be materialized in the Validator
@@ -33,9 +30,9 @@ describe("Starting a Fork", function () {
     const db = getDatabase(signer);
 
     // `key` is a reserved word in sqlite.
-    const res = await db.prepare(
-      "SELECT * FROM pilot_sessions_1_7 LIMIT 10;"
-    ).all();
+    const res = await db
+      .prepare("SELECT * FROM pilot_sessions_1_7 LIMIT 10;")
+      .all();
 
     expect(res.success).to.eql(true);
     expect(res.results.length).to.eql(10);
@@ -43,11 +40,12 @@ describe("Starting a Fork", function () {
     const sessionOne = res.results[0];
     expect(sessionOne.end_time).to.eql(16141361);
     expect(sessionOne.id).to.eql(1);
-    expect(sessionOne.owner).to.eql("0xdd8a5674eca6f1367bd0398d4b931d5b351c0be6");
+    expect(sessionOne.owner).to.eql(
+      "0xdd8a5674eca6f1367bd0398d4b931d5b351c0be6"
+    );
     expect(sessionOne.pilot_contract).to.eql(null);
     expect(sessionOne.pilot_id).to.eql(null);
     expect(sessionOne.rig_id).to.eql(1018);
     expect(sessionOne.start_time).to.eql(15967006);
-
   });
 });
