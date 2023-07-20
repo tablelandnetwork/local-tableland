26a0d81f30a7f9d7811f6b1c2a9118a1832a1c4c
Dan Buchholz <dbuchholz30@gmail.com>
2023-07-19 22:25:25 -0700

    Revert "fix: use absolute paths; add timout"
    This reverts commit cc7e83e2b1f80fe76fa3e3ce99b68ca9b0b0d9ec.

--- test/cli.test.mjs
+++ test/cli.test.mjs
@@ -1,79 +1,61 @@
 import chai from "chai";
 import shell from "shelljs";
-import { join } from "path";
 
 const expect = chai.expect;
 
 describe("Project builder", function () {
-  // Store absolute paths to the current working directory & path to tmp dir
-  // Running on containers can cause unexpected behavior with relative paths
-  const cwd = process.cwd();
-  const pathToTmp = join(cwd, "tmp");
-
-  // Tests need some extra time running `up.js` (async CLI inputs & spins up LT)
-  this.timeout(12000);
   beforeEach(function () {
-    // Create tmp dir to run the project builder in
-    shell.rm("-rf", pathToTmp);
-    shell.mkdir("-p", pathToTmp);
-    shell.cd(pathToTmp);
+    // create tmp dir to run the project builder in
+    shell.rm("-rf", "tmp");
+    shell.mkdir("-p", "tmp");
+    shell.cd("tmp");
   });
 
   afterEach(function () {
-    // Cleanup tmp dir
-    shell.cd(join(pathToTmp, ".."));
-    shell.rm("-rf", pathToTmp);
+    // cleanup
+    shell.cd("..");
+    shell.rm("-rf", "tmp");
   });
 
   it("should create a config file", async function () {
     /* eslint-disable node/no-unsupported-features/es-syntax */
-    // Pass input from `choose-yes.txt` to `up.js` to answer `y` to prompt
-    shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
 
-    // Import the created config file & example config file
-    const createdFile = await import(join(pathToTmp, "tableland.config.js"));
-    const exampleFile = await import(
-      join(pathToTmp, "../dist/esm/tableland.config.example.js")
-    );
+    // The file `choose-yes.txt` has the key stokes needed that would
+    // go through the prompt and choose yes for the question being asked
+    shell.cat("../test/choose-yes.txt").exec("../dist/esm/up.js --init");
 
-    // Ensure the files are the same (i.e., use same default values)
+    const createdFile = await import("../tmp/tableland.config.js");
+    const exampleFile = await import("../dist/esm/tableland.config.example.js");
+
     expect(createdFile).to.eql(exampleFile);
   });
 
-  it("should do nothing if config file already exists", async function () {
-    // Pass input from `choose-yes.txt` to `up.js` to answer `y` to prompt
-    shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
+  it("should do nothing if config file already exists", function () {
+    // run the cli to create a file
+    shell.cat("../test/choose-yes.txt").exec("../dist/esm/up.js --init");
 
-    // Check the file was created in the tmp dir
-    const wasCreated = shell.ls(pathToTmp);
+    const wasCreated = shell.ls("../tmp");
     expect(wasCreated[0]).to.eql("tableland.config.js");
 
-    // Try to run it again, which should do nothing due to file existing
+    // run the cli again and it should fail because config exists already
     const cliOut = shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
+      .cat("../test/choose-yes.txt")
+      .exec("../dist/esm/up.js --init");
 
-    // Config file should already exist
     expect(cliOut.toString()).to.match(
       /Config file already exists, nothing to do/
     );
   });
 
   it("should do nothing if user says not to", async function () {
-    // Pass input from `choose-no.txt` to `up.js` to answer `yn to prompt
+    /* eslint-disable node/no-unsupported-features/es-syntax */
     const cliOut = shell
-      .cat(join(pathToTmp, "../test/choose-no.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
+      .cat("../test/choose-no.txt")
+      .exec("../dist/esm/up.js --init");
 
-    // `stdout` should include message
     expect(cliOut.toString()).to.match(/run this again anytime/);
 
-    // No file should have been created
-    expect(import(join(pathToTmp, "tableland.config.js"))).to.be.rejectedWith(
+    await expect(import("../tmp/tableland.config.js")).to.be.rejectedWith(
       "Cannot find module"
     );
   });
