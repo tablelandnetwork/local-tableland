078229bab7fc921304b721bf5936d76aec003dc7
Dan Buchholz <dbuchholz30@gmail.com>
2023-07-20 15:18:34 -0700

    fix: join paths properly

--- test/cli.test.mjs
+++ test/cli.test.mjs
@@ -29,13 +29,12 @@ describe("Project builder", function () {
     /* eslint-disable node/no-unsupported-features/es-syntax */
     // Pass input from `choose-yes.txt` to `up.js` to answer `y` to prompt
     shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
-
+      .cat(join(pathToTmp, "..", "test", "choose-yes.txt"))
+      .exec(join(pathToTmp, "..", "dist", "esm", "up.js --init"));
     // Import the created config file & example config file
     const createdFile = await import(join(pathToTmp, "tableland.config.js"));
     const exampleFile = await import(
-      join(pathToTmp, "../dist/esm/tableland.config.example.js")
+      join(pathToTmp, "..", "dist", "esm", "tableland.config.example.js")
     );
 
     // Ensure the files are the same (i.e., use same default values)
@@ -45,17 +44,16 @@ describe("Project builder", function () {
   it("should do nothing if config file already exists", async function () {
     // Pass input from `choose-yes.txt` to `up.js` to answer `y` to prompt
     shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
-
+      .cat(join(pathToTmp, "..", "test", "choose-yes.txt"))
+      .exec(join(pathToTmp, "..", "dist", "esm", "up.js --init"));
     // Check the file was created in the tmp dir
     const wasCreated = shell.ls(pathToTmp);
     expect(wasCreated[0]).to.eql("tableland.config.js");
 
     // Try to run it again, which should do nothing due to file existing
     const cliOut = shell
-      .cat(join(pathToTmp, "../test/choose-yes.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
+      .cat(join(pathToTmp, "..", "test", "choose-yes.txt"))
+      .exec(join(pathToTmp, "..", "dist", "esm", "up.js --init"));
 
     // Config file should already exist
     expect(cliOut.toString()).to.match(
@@ -66,8 +64,8 @@ describe("Project builder", function () {
   it("should do nothing if user says not to", async function () {
     // Pass input from `choose-no.txt` to `up.js` to answer `yn to prompt
     const cliOut = shell
-      .cat(join(pathToTmp, "../test/choose-no.txt"))
-      .exec(join(pathToTmp, "../dist/esm/up.js --init"));
+      .cat(join(pathToTmp, "..", "test", "choose-no.txt"))
+      .exec(join(pathToTmp, "..", "dist", "esm", "up.js --init"));
 
     // `stdout` should include message
     expect(cliOut.toString()).to.match(/run this again anytime/);
