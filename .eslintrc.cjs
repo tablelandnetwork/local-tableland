14135e8b245a703e810829046ffef266d7186dea
Dan Buchholz <dbuchholz30@gmail.com>
2023-07-24 14:17:25 -0700

    chore: revert changes to eslint/prettier for validator config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,14 +21,6 @@ module.exports = {
     "plugin:prettier/recommended",
     "plugin:node/recommended",
   ],
-  overrides: [
-    {
-      files: ["validator/config.json"],
-      rules: {
-        "eol-last": "off",
-      },
-    },
-  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   parserOptions: {
